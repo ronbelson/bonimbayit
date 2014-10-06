79248d33ba323d3165eda5530821706a8eededec
Ron Belson <ronbelson@gmail.com>
2014-10-06 09:19:37 +0300

    update front page to node.js and jade views

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'שלום רון' });
+router.get('/', function(req, res) { 
+  res.render('index', { title: 'בונים בית,יומן הבנייה המקיף בישראל' });
 
 });
 
