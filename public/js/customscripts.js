84e9c35cd6fd2c92b53953851e9caf0f4633f7be
Ron Belson <ronbelson@gmail.com>
2014-10-08 02:03:13 +0300

    modal like update to close

--- public/js/customscripts.js
+++ public/js/customscripts.js
@@ -1,13 +1,11 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '631527010302196',
+      xfbml      : true,
+      version    : 'v2.1'
+    });
 
- (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/he_IL/sdk.js#xfbml=1&appId=&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-if ($(this).width() > 768 && $.cookie("fb-back1")==undefined)  {
+      if ($(this).width() > 768 && $.cookie("fb-back12")==undefined)  {
 
         $.cookie("fb-back1", "Aurelio", {expires: new Date(2029, 10, 29, 11, 00, 00)});
         setTimeout(function() {
@@ -15,6 +13,27 @@ if ($(this).width() > 768 && $.cookie("fb-back1")==undefined)  {
         }, 1000); // milliseconds
 
     };
+      
+      FB.Event.subscribe('edge.create', function(response) {
+         $('#fb-back').modal('hide');
+      }); 
+
+      FB.Event.subscribe('edge.remove', function(response) {
+         $('#fb-back').modal('hide');
+      }); 
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));  
+
+ 
+
+
 
 
      
