2a38bd730d9fd88468b5d2ed2f0142856ac7f6f9
Ron Belson <ronbelson@gmail.com>
2014-10-07 22:06:52 +0300

    master is bug

--- public/js/front-page.js
+++ public/js/front-page.js
@@ -1,19 +1,17 @@
+$(function(){
+var items = (Math.floor(Math.random() * ($('#testimonials li').length)));
+$('#testimonials li').hide().eq(items).show();
 
+function next(){
+  $('#testimonials li:visible').delay(5000).fadeOut('slow',function(){
+    $(this).appendTo('#testimonials ul');
+    $('#testimonials li:first').fadeIn('slow',next);
+  });
+ }
+next();
+});
 
 $(document).ready(function () {
-
-	$(function(){
-		var items = (Math.floor(Math.random() * ($('#testimonials li').length)));
-		$('#testimonials li').hide().eq(items).show();
-
-		function next(){
-		  $('#testimonials li:visible').delay(5000).fadeOut('slow',function(){
-		    $(this).appendTo('#testimonials ul');
-		    $('#testimonials li:first').fadeIn('slow',next);
-		  });
-		 }
-		next();
-		});
 
     //setTimeout(function() {
       //$('#fb-back').modal('show');
