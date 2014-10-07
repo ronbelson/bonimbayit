a87c72d5222d8dee454dddc690829fe409650857
Ron Belson <ronbelson@gmail.com>
2014-10-07 21:18:39 +0300

    bug with fb like.

--- public/js/front-page.js
+++ public/js/front-page.js
@@ -1,23 +1,25 @@
-$(function(){
-var items = (Math.floor(Math.random() * ($('#testimonials li').length)));
-$('#testimonials li').hide().eq(items).show();
 
-function next(){
-  $('#testimonials li:visible').delay(5000).fadeOut('slow',function(){
-    $(this).appendTo('#testimonials ul');
-    $('#testimonials li:first').fadeIn('slow',next);
-  });
- }
-next();
+
+$(document).ready(function () {
+
+	$(function(){
+		var items = (Math.floor(Math.random() * ($('#testimonials li').length)));
+		$('#testimonials li').hide().eq(items).show();
+
+		function next(){
+		  $('#testimonials li:visible').delay(5000).fadeOut('slow',function(){
+		    $(this).appendTo('#testimonials ul');
+		    $('#testimonials li:first').fadeIn('slow',next);
+		  });
+		 }
+		next();
+		});
+
+    //setTimeout(function() {
+      //$('#fb-back').modal('show');
+    //}, 1000); // milliseconds
+
 });
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/he_IL/sdk.js#xfbml=1&appId=182829178418239&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
 
   
