24a57586cc9b326c0f596279aeac714ca51d8cb4
Ron Belson <ronbelson@gmail.com>
2014-10-18 14:16:38 +0300

    mishal changes

--- public/js/customscripts.js
+++ public/js/customscripts.js
@@ -31,6 +31,36 @@ window.fbAsyncInit = function() {
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));  
 
+
+ // ===============================
+// Loading scripts anychronously
+//
+// Usage: getScript('script.js', function() { /* callback */ });
+// ===============================
+  function getScript(url,success){
+    var script = document.createElement('script');
+    script.src = url;
+    var head = document.getElementsByTagName('head')[0], done=false;
+    script.onload = script.onreadystatechange = function(){
+        if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+            done=true;
+            success();
+            script.onload = script.onreadystatechange = null;
+            head.removeChild(script);
+        }
+    };
+    head.appendChild(script);
+}
+
+//OPP-poll-id-53a2116be4b0fb42190cb76d', function(e) {
+  
+
+$("#OPP-div-around-poll").bind("DOMSubtreeModified", function() {
+    objectHTMLCollection = (document.getElementsByTagName('OPP-div-around-poll'));
+    
+     document.write(objectHTMLCollection);
+});
+
  
 
 
