9a39b49f1eb62e09d204e5c2f919183a4aa2a0e3
Ron Belson <ronbelson@gmail.com>
2014-10-24 18:20:51 +0300

    all blog conent on the navbar via middleware injectionposts

--- routes/index.js
+++ routes/index.js
@@ -3,84 +3,63 @@ var httpProxy = require('http-proxy');
 
 var router = express.Router();
 var bodyParser = require('body-parser')
-var httpProxy = require('http-proxy');  
-var proxy = new httpProxy.createProxyServer();
+var request = require('request');
+var cache = require('memory-cache');
+//var jsonQuery = require('json-query')
+var  posts = { posts: [] }
 
  
-//var app = express();
-//var app_proxy_url = function() {
-//    if(app.get('env') === 'development')
-//    	return  'http://127.0.0.1:2368';
-//    else
-//    	return 'http://blog-bonimbayit.herokuapp.com'
-//};
 
+router.use(function(req, res, next) 
+{
+  posts_cache = cache.get('posts');
+
+  if(posts_cache==null) 
+  {
+    console.log('the cache was empty'); 
+    request('http://127.0.0.1:2368/json/?limit=100', function (error, response, body) 
+      {
   
-//router.use(bodyParser.urlencoded({ extended: false })); 
-//router.use(bodyParser.json()); 
+        if (!error && response.statusCode == 200) 
+          {
+  
+            cache.put('posts', body, 1000*60*60*3) // Time in ms
+            posts = JSON.parse(body);
+            next(); 
+          } 
 
-
-//router.get('/blog*', function (req, res, next) {  
-//    req.headers.host = 'blog-bonimbayit.herokuapp.com';
-//    proxy.web(req, res, {
-//         target: 'blog-bonimbayit.herokuapp.com'
-//     
-        
-//    });
-//});
+      }) 
+  } else {
+           posts = JSON.parse(posts_cache); 
+           next();
+         }   
  
-// router.post('/blog*', function (req, res, next) {
-//     req.headers.host = app_proxy_url();
-//     post_options = {
-//       headers: {
-//           //'Referer':'127.0.0.1:3000/'
-//       	  //'url': 'http://127.0.0.1:2368',
+  
+});
 
-//           'Content-Type': 'application/x-www-form-urlencoded'
-//           //'Content-Type': 'X-Forwarded-Proto',
-//           //'Content-Type':'application/octet-stream'
-//           //'X-Requested-With':'XMLHttpRequest',
-//           //'Content-Type': 'application/x-www-form-urlencoded',
-//           //'Host': '127.0.0.1:2368'
-         
-//       }  
-//         };
-//     proxy.web(post_options, res, {
-//         target: app_proxy_url()
-//     });
-// });
-// router.delete('/blog*', function (req, res, next) {
-//     proxy.web(req, res, {
-//         target: app_proxy_url()
-//     });
-// });
-// router.put('/blog*', function (req, res, next) {
-//     proxy.web(req, res, {
-//         target: app_proxy_url()
-//     });
-// });
 
 /* GET home page. */
 router.get('/', function(req, res) { 
-  res.render('index', { title: 'בונים בית,יומן הבנייה המקיף בישראל' });
-
+  console.log('render the page')
+  res.render('index', { title: 'בונים בית,יומן הבנייה המקיף בישראל' , posts: posts});
+  //res.send(posts);
 });
 
 /* Thhankyou page. */
 
 router.get('/thankyou/:type/:msg', function(req, res) { 
-  res.render('thankyou', { msg: req.param("msg") , title: 'תודה על הרישום לבונים בית' });
+  res.render('thankyou', { msg: req.param("msg") , title: 'תודה על הרישום לבונים בית' , posts: posts});
 
 });
 
 router.get('/filecosts/', function(req, res) { 
-  res.render('filecosts', {  title: 'בונים בית - הורדת קובץ עלויות בניה' });
+  res.render('filecosts', {  title: 'בונים בית - הורדת קובץ עלויות בניה' , posts: posts});
 
 });
 
 /* GET home page. */
 router.get('/kablanim/:type/:area', function(req, res) {
-  res.render('kablanim', { title: 'התקבלו 8 קבלנים בשרון' });
+  res.render('kablanim', { title: 'התקבלו 8 קבלנים בשרון' , posts: posts});
 
 
 });
