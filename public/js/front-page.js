febe33de32635f8dccd06bf6e3c2bb5261616830
Ron Belson <ronbelson@gmail.com>
2014-11-01 21:44:23 +0200

    background.jpg

--- public/js/front-page.js
+++ public/js/front-page.js
@@ -4,7 +4,7 @@ $('#div_testimonials').show();
 $('#testimonials li').hide().eq(items).show();
 
 function next(){
-  $('#testimonials li:visible').delay(5000).fadeOut('slow',function(){
+  $('#testimonials li:visible').delay(7000).fadeOut('slow',function(){
     $(this).appendTo('#testimonials ul');
     $('#testimonials li:first').fadeIn('slow',next);
   });
